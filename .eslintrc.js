797cadae7788f9762fe51a53f452ec4e864a2325
Beryl <mniberyl@gmail.com>
2020-11-19 01:46:06 +0900

    Add eslint packages

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2021": true
+  },
+  "plugins": [
+    "editorconfig",
+    "import",
+    "html",
+  ],
+  "extends": "airbnb-base",
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "rules": {
+    "import/no-extraneous-dependencies": 0,
+    "import/no-unresolved": [2, {
+      ignore: ['^@/'],
+    }],
+    "import/prefer-default-export": 0,
+    "no-console": [1, {
+      allow: ["warn", "error"],
+    }],
+    "no-nested-ternary": 0,
+  }
+};
